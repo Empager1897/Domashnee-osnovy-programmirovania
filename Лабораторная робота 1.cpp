4618b2da05d9d9581f4d81cca06942bf62e5e495
NyvchykPetr <pnivchik@gmail.com>
2021-09-15 18:01:23 +0300

    Add files via upload

---
+++ Лабораторная робота 1.cpp
@@ -0,0 +1,17 @@
+﻿#include <math.h>
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+	float a,S;
+	cout << "vvedite chislo" << endl;
+	cin >> a;
+	S = (pow(a, 2) * sqrt(3)) / 4;
+	cout << "Ploshad ravna=" << S;
+
+
+
+}
+
