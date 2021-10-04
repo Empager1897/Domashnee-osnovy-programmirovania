850ea5ab3e4f966f7587312d7c17f781f5959a5c
NyvchykPetr <pnivchik@gmail.com>
2021-10-04 16:34:55 +0300

    Add files via upload

---
+++ lab2Python.py
@@ -0,0 +1,8 @@
+x=float(input())
+y=float(input())
+if x>=2:
+    print('tochka prinadlezhit ploskosti')
+    elif ((x+2)**2+y**2)<4 and (x+2)**2+y**2>=1:
+        print('tochka prinadlezhit ploskosti')
+        else:
+            print('tochka ne prinadlezhit ploskosti')
