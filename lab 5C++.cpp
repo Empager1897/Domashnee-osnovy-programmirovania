966370ac60e11084b13a642e88c4827da828e72b
NyvchykPetr <pnivchik@gmail.com>
2021-10-31 08:19:28 +0200

    Add files via upload

---
+++ lab 5C++.cpp
@@ -0,0 +1,24 @@
+﻿#include <windows.h>
+#include <math.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int i = 10000,k=2,s=0;
+    for (i; i < 100000; i++) {
+        if(i/10000==i%10&&i/1000%10==i/10%10){
+            for (k; k <= i / 2; k++) {
+                if (i % k == 0) {
+                    break;
+                }
+            }
+            cout << i << " Простой палиндром";
+        }
+        s++;
+        if (s == 10) {
+            break;
+        }
+    }
+}
+
